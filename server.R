48dd8eb0a48ffa1997a640d7936dbfb8aeadb77e
jvcasill <casillas@dhcp-10-132-151-191.uawifi.arizona.edu>
2014-12-18 10:00:42 -0700

    added math, model sum, logistic regression desc

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-library(shiny)
+library(shiny); library(stargazer)
 
 crossOver <- function(x) {
     cross <- (summary(x)$coefficients[1, 1] / summary(x)$coefficients[2, 1] * -1)
@@ -11,7 +11,7 @@ shinyServer(function(input, output) {
         set.seed(1)
         
         x       = rnorm(1300, 1, 3)
-        linpred = input$intercept + x * input$beta
+        linpred = input$intercept + x * input$slope
         prob    = exp(linpred)/(1 + exp(linpred))
         runis   = runif(1300, 0, 1)
         y       = ifelse(runis < prob, 1, 0)
@@ -25,23 +25,53 @@ shinyServer(function(input, output) {
     
         plot(fit$fitted.values ~ stim, data = df, type = 'n', 
         xaxt = 'n', xlab = "VOT", yaxt = 'n', ylab  = "", 
-        main = "Phoneme boundary for /ba/ - /pa/ continuum")
+        main = "Phoneme boundary for /b/ - /p/ continuum")
         curve(predict(fit, data.frame(stim = x),type="resp"), 
               add = TRUE, lty = 1, lwd = 1.5, 
               col = rgb(0, 0, 204, 102, maxColorValue = 255)) 
         abline(v = cop, h = 0.5, 
                col = rgb(150, 0, 204, 102, maxColorValue = 255))
         abline(h = 0.5, col = 'grey')
-        axis(2, at = c(0, 0.5, 1), label = c("/b/", "0.5", "/p/"), 
+        axis(2, at = c(0, 0.5, 1), label = c("big", "50%", "pig"), 
              las = 2)
         axis(1, at = seq(-60, 60, by = 10), label = seq(-60, 60, 
                                                         by = 10))
     })
 
+    output$modelSum <- renderPrint({
+        set.seed(1)
+        x       = rnorm(1300, 1, 3)
+        linpred = input$intercept + x * input$slope
+        prob    = exp(linpred)/(1 + exp(linpred))
+        runis   = runif(1300, 0, 1)
+        y       = ifelse(runis < prob, 1, 0)
+        df <- data.frame(prop = y, x = x)
+        df <- df[with(df, order(x)), ]
+        df$stim <- rep(seq(-60, 60, by = 10), each = 100)
+        fit  <- glm(prop ~ x, data = df, family = 'binomial')
+        stargazer(fit, type = 'html', single.row=TRUE, 
+                  dep.var.labels="Proportion 'pig'", 
+                  covariate.labels=c("VOT", "Intercept"),
+                  omit.stat=c("n", "aic"), ci=TRUE, ci.level=0.95, 
+                  align=FALSE)
+    })
+    
+    output$space <- renderText({
+        paste0("---")
+    })
+    
+    output$intEst <- reactive({
+        paste0("The intercept is: ", input$intercept, ".")
+    })
+
+    output$slopeEst <- reactive({
+        paste0("The slope is: ", input$slope, ".")
+    })
+    
     output$crossOP <- renderText({
         set.seed(1)
         x       = rnorm(1300, 1, 3)
-        linpred = input$intercept + x * input$beta
+        linpred = input$intercept + x * input$slope
         prob    = exp(linpred)/(1 + exp(linpred))
         runis   = runif(1300, 0, 1)
         y       = ifelse(runis < prob, 1, 0)
--- ui.R
+++ ui.R
@@ -9,13 +9,17 @@ shinyUI(pageWithSidebar(
         h3("Details:"),
         p("This application utilizes the function crossOver in 
           order to calculate the 50% crossover point of a 
-          participants perceptual boundary for a /ba/-/pa/ 
-          continuum. You can adjust the intercept and slope of the 
+          participants perceptual boundary for a /b/-/p/ 
+          continuum. The application fits a linear 
+          function to the log odds of selecting 'pig'."),
+        withMathJax(helpText("$$\\log(\\frac{p}{1 - p}) = 
+                             \\beta_0 + \\beta_1VOT + \\epsilon$$")),
+        p("You can adjust the intercept and slope of the 
           model in real time to obtain an updated calculation of 
           the crossover point in ms."),
-        sliderInput("intercept", "Intercept", min = -11.0, 
+        sliderInput("intercept", "$$\\textrm{Intercept}\\: (\\,\\beta_0\\,)$$", min = -11.0, 
                     max = 6.0, value = -5.0, step = .25),
-        sliderInput("beta", "Slope", min = 2.25, max = 10, 
+        sliderInput("slope", "$$\\textrm{Slope}\\: (\\,\\beta_1VOT\\,)$$", min = 2.25, max = 10, 
                     value = 2.75, step = .25),
         h5("Created by:"),
         tags$a("Joseph V. Casillas", 
@@ -27,7 +31,10 @@ shinyUI(pageWithSidebar(
 
     mainPanel(        
         plotOutput("logPlot"),
-        #textOutput("text"),
+        tableOutput("modelSum"),
+        tableOutput("space"),
+        tableOutput("intEst"),
+        tableOutput("slopeEst"),
         tableOutput("crossOP")
     )
 ))
